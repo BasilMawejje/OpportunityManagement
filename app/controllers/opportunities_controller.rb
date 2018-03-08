62a9354be29f4b13c33fc8cdef748367d330863d
BasilMawejje <basil.mawejje@gmail.com>
2018-03-08 11:59:13 +0300

    Add opportunity controller and view Add association between opportunity and account

--- app/controllers/opportunities_controller.rb
+++ app/controllers/opportunities_controller.rb
@@ -1,2 +1,22 @@
 class OpportunitiesController < ApplicationController
+  def index
+    @opportunities = Opportunity.all
+  end
+
+  def new
+    @opportunity = Opportunity.new
+    @accounts = Account.all.map{|c| [ c.account_name, c.id ] }
+  end
+
+  def create
+    @opportunity = Opportunity.new(opportunity_params)
+    @opportunity.account_id = params[:account_id]
+    @opportunity.save
+    redirect_to opportunities_path
+  end
+
+  private
+    def opportunity_params
+      params.require(:opportunity).permit(:opp_name, :opp_description, :amount)
+    end
 end
